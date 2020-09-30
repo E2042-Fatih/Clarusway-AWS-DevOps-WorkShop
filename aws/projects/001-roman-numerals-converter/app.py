d92e3f93aee9e185b4e8269dbf5a3a3dc882b8c7
sean <seantuny20@gmail.com>
2020-09-30 21:28:51 +0300

    the most last changes

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -1,16 +1,15 @@
 
-
 from flask import Flask, render_template, request , redirect, url_for
 
 app = Flask (__name__)
 
-@app.route("/")
-def home ():
+@app.route("/", methods = ['GET'])
+def main_get ():
     return render_template ("index.html", not_valid = False, developer_name = "E2042-Fatih")
 
 
-@app.route ("/", methods = ["GET","POST"])
-def calc ():
+@app.route ("/", methods = ["POST"])
+def main_post ():
     if request.method == "POST":
         number = request.form["number"]
         if number.isdigit() == False or not 0<int(number)<4000:
@@ -37,8 +36,55 @@ def calc ():
             
         return render_template("result.html", number_decimal=number , number_roman = conv(number) , developer_name = "E2042-Fatih")
 
-
 if __name__ == "__main__":
-
     app.run(debug = True)
     #app.run(host = '0.0.0.0', port = 80)
+
+
+# farklı çözüm yöntemi-1
+# def convert(decimal_num):
+#    roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+#    num_to_roman = ''
+#    for i in roman.keys():
+#        num_to_roman += roman[i]*(decimal_num//i)
+#        decimal_num %= i
+#    return num_to_roman
+
+# farklı çözü yöntemi-2
+#def convert_to_roman(num):
+#    roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+#	sayi = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+#    romanvalue = ""
+#    for i,d in enumerate(sayi):
+#        while (num >= d):
+#            num -= d
+#            romanvalue += roman[i]
+#    return romanvalue
+#print(convert_to_roman(3100))
+
+# farklı çözü yöntemi-3
+#def convert(num):
+#    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+#    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+#    number_roman = ''
+#    i = 0
+#    while  num > 0:
+#        for _ in range(num // val[i]):
+#            number_roman += syb[i]
+#            num -= val[i]
+#        i += 1
+#    return (number_roman)
+
+# farklı çözü yöntemi-4
+#def InttoRoman(number):
+#    romandict = {'M':1000, 'CM':900, 'D':500, 'CD':400, 'C':100, 'XC':90, 'L':50, 'XL':40, 'X':10, 'IX':9, 'V': 5, 'IV':4, 'I':1}
+#    if (not number.isdigit()) or ((int(number) > 3999) or (int(number) < 1)):
+#        return 0
+#    number = int(number)    
+#    result = ""
+#    for key, value in romandict.items():
+#        while number >= value:
+#            quotient = number // value 
+#            result += key * quotient
+#            number %= value
+#    return result
